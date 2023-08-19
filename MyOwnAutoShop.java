03df61f00a224ce27089775b16bd4be5d55a50a7
John Doe <betiaman@example.com>
2023-08-19 02:30:50 -0500

    complete sba

--- MyOwnAutoShop.java
+++ MyOwnAutoShop.java
@@ -56,3 +56,41 @@ class Ford extends Car {
         return super.getSalePrice() - manufacturerDiscount;
     }
 }
+
+// Requirement 4: Create a subclass called Sedan with additional fields and methods
+class Sedan extends Car {
+    int length;
+
+    // Constructor for initializing Sedan fields, including super constructor call
+    public Sedan(int speed, double regularPrice, String color, int length) {
+        super(speed, regularPrice, color);
+        this.length = length;
+    }
+
+    // Method to calculate the sale price with discount based on length
+    @Override
+    public double getSalePrice() {
+        if (length > 20) {
+            return super.getSalePrice() * 0.95; // 5% discount
+        } else {
+            return super.getSalePrice() * 0.9; // 10% discount
+        }
+    }
+}
+
+// Requirement 5: Create the MyOwnAutoShop class with the main method
+public class MyOwnAutoShop {
+    public static void main(String[] args) {
+        // Create instances of different classes and display sale prices
+        Sedan hyperStreamSedan = new Sedan(150, 20000, "Red", 25);
+        Ford fordGalacticCruiser = new Ford(180, 30000, "Blue", 2023, 2000);
+        Ford fordQuantumX = new Ford(160, 25000, "Green", 2022, 1500);
+        Car NeuroDriveCar = new Car(140, 18000, "Black");
+
+        System.out.println("Sale price for Hyper Stream Sedan: $" + hyperStreamSedan.getSalePrice());
+        System.out.println("Sale price for Ford Galactic Cruiser: $" + fordGalacticCruiser.getSalePrice());
+        System.out.println("Sale price for Ford QuantumX: $" + fordQuantumX.getSalePrice());
+        System.out.println("Sale price for NeuroDriveCar: $" + NeuroDriveCar.getSalePrice());
+    }
+} 
+
